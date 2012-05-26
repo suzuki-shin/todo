6d5266e997a5cb7074b609994fd6da8abb77868e
seizans <seizans@gmail.com>
2012-05-26 14:10:44 +0900

    Todo新規作成ページを作成.
    - formとform用widgetを実装.
    - Boolフィールド用にselectListを利用.
    - Dayフィールドを実装.
        - Data.Time (Day) をimportする.
        - .cabalファイルにtimeライブラリへの依存関係を記述する.
    - Dayフィールド用にJquery部品を実装.
        - Foundation.hs で App を YesodJqueryインスタンス化する.
        - Handler内でYesod.Form.Jqueryをimportして使う.
    - config/models の default=homu は、カラム追加等のMigration時に意味あり.

--- Application.hs
+++ Application.hs
@@ -24,6 +24,7 @@ import Network.HTTP.Conduit (newManager, def)
 -- Import all relevant handler modules here.
 import Handler.Home
 import Handler.Tag
+import Handler.Todo
 
 -- This line actually creates our YesodSite instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see
